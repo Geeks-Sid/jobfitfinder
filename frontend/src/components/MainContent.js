6c90da42dd38e0633eeb6adb3e2c6df5b59a2c62
Geeks-Sid <sid.cre8er@gmail.com>
2023-10-05 17:20:07 -0400

    saving failed

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainContent.css';
 
 const MainContent = () => {
+  const [isScanning, setIsScanning] = useState(false);
+
+  const handleFileUpload = (event) => {
+    // Logic for file upload can go here
+    console.log("File uploaded");
+  };
+
+  const handleScanClick = () => {
+    if (isScanning) return;  // Don't re-initiate if already scanning
+
+    setIsScanning(true);
+    
+    setTimeout(() => {
+      setIsScanning(false);
+    }, 3000); // Simulate a 3-second scan operation
+  };
+
   return (
     <main>
-      <input type="file" id="resume" name="resume" />
-      <button>Scan Resume</button>
+      <input type="file" id="resume" name="resume" onChange={handleFileUpload} />
+      <button onClick={handleScanClick}>
+        {isScanning ? 'Scanning...' : 'Scan Resume'}
+      </button>
+      {isScanning && <p>Scanning...</p>}
+      {!isScanning && <p>Ready to scan.</p>}
     </main>
   );
 }
